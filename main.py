39ce157e8a3b89ee69bc0d97e3513c48c3c3f6ba
Coockieinyourass <Gatindanis45@gmail.com>
2025-10-09 20:40:46 +0300

    spot naming added

--- main.py
+++ main.py
@@ -2,32 +2,46 @@ from tkinter import *
 
 
 
-SCALE = 20
+SCALE = 40
 XPADDING = SCALE
 YPADDING = SCALE
 
 
 
-def draw_spot(H, V, color):
+def draw_spot(H, V, color, name):
     canvas.create_oval(XPADDING + 10*SCALE - H*SCALE - SCALE/5, # Генерация точки по x и y координатам
                         YPADDING + 10*SCALE - V*SCALE - SCALE/5, 
                         XPADDING + 10*SCALE - H*SCALE + SCALE/5,
                         YPADDING + 10*SCALE - V*SCALE + SCALE/5,
                         fill=color, tags="spot")
+    if(color == "#f54242"):
+        canvas.create_text(XPADDING + 9.7*SCALE - H*SCALE,
+                        YPADDING + 9.7*SCALE - V*SCALE,
+                        text=f"{name}2")
+    elif(color == "#5af542"):
+        canvas.create_text(XPADDING + 9.7*SCALE - H*SCALE,
+                        YPADDING + 9.7*SCALE - V*SCALE,
+                        text=f"{name}1")
+    elif(color == "#424bf5"):
+        canvas.create_text(XPADDING + 9.7*SCALE - H*SCALE,
+                        YPADDING + 9.7*SCALE - V*SCALE,
+                        text=f"{name}3")
 
 def take_cords():
     X = float(cords_taker_x.get())
-    print(cords_taker_x.get())
+    # print(X)
     Y = float(cords_taker_y.get())
-    print(cords_taker_y.get())
+    # print(Y)
     Z = float(cords_taker_z.get())
-    print(cords_taker_z.get())
+    # print(Z)
+    name = spot_name_taker.get()
+    # print(name)
 
-    print(f"Координаты получены: x = {cords_taker_x.get()}; y = {cords_taker_y.get()}; z = {cords_taker_z.get()};")
+    print(f"Координаты получены: x = {X};\ny = {Y};\nz = {Z};\nимя точки - {name}")
 
-    draw_spot(X, Z, "#f54242")
-    draw_spot(X, -Y, "#5af542")
-    draw_spot(-Y, Z, "#424bf5")
+    draw_spot(X, Z, "#f54242", name)
+    draw_spot(X, -Y, "#5af542", name)
+    draw_spot(-Y, Z, "#424bf5", name)
 
 def clear_spots():
     canvas.delete("spot")
@@ -75,10 +89,18 @@ x_title.pack(side="left")
 cords_taker_x = Entry(frame_x)
 cords_taker_x.pack(side="left")
 
+frame_spot_name = Frame(root)
+frame_spot_name.pack(anchor="se", side="bottom", padx=XPADDING, pady=YPADDING/5)
+spot_name_title = Label(frame_spot_name, text="Имя точки: ")
+spot_name_title.pack(side="left")
+spot_name_taker = Entry(frame_spot_name)
+spot_name_taker.pack(side="left")
+
+
 
 
 canvas = Canvas(bg="white", width=22*SCALE, height=22*SCALE) # Канвас...
-canvas.pack(anchor="c", pady=5*SCALE, padx=5*SCALE)
+canvas.pack(anchor="sw")
 
 canvas.create_line(XPADDING+10*SCALE, YPADDING, XPADDING+10*SCALE, YPADDING+20*SCALE, width=3.5) # Основные линии координат
 canvas.create_line(XPADDING, YPADDING+10*SCALE, XPADDING+20*SCALE, YPADDING+10*SCALE, width=3.5)
